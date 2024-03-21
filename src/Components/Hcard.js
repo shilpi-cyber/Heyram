50e58bb0929411b46d2449ef832d8a0cc6585bb2
shilpi-cyber <shilpi.kaushik9@gmail.com>
2024-03-21 16:46:12 +0530

    hover

--- src/Components/Hcard.js
+++ src/Components/Hcard.js
@@ -10,8 +10,7 @@ function Hcard({ input }) {
             <Card className='body-c m-2' style={{ height: "28rem" , border:"1px solid white" , padding: "1px"}} >
                 <Card.Img variant="top" src={input.img} />
                 <Card.Body className='text-start'>
-                    <Card.Title>{input.name}</Card.Title>
-
+                    <Card.Title className='ttl'>{input.name}</Card.Title>
                     <Card.Text>
                         {input.description}
                         <Zoom><Link to={input.path}></Link></Zoom>
